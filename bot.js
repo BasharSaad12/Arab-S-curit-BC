f4a38052971d28b8531cf1ff381c16a3187a2a0e
BasharSaad12 <47093295+BasharSaad12@users.noreply.github.com>
2020-12-08 10:50:08 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,4 +61,4 @@ client.on('message', message => { // هاذا للبرودكسات
 
 
 
-client.login(process.env.Nzg1OTMzMjg1NTQwMjMzMjY2.X8_DaQ.R-r1Fu7E_lHzGII6v_MEIzbGGHo);
+client.login(process.env.Nzg1OTMzMjg1NTQwMjMzMjY2.X8_DaQ.Rr1Fu7E_lHzGII6v_MEIzbGGHo);
